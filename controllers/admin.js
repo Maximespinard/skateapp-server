569b439795d227728941e87326dbc98e300a640d
mspinard <mspinard@vulog.com>
2021-11-02 17:41:48 +0100

    log admin

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
 import Admin from '../models/admin.js';
 
 const router = express.Router();
+dotenv.config();
+
+const secret = process.env.SECRET;
 
 export const addAdmin = async (req, res) => {
   try {
@@ -16,14 +20,45 @@ export const addAdmin = async (req, res) => {
       return res.status(400).json({ message: "L'admin existe déjà" });
 
     const hashedPassword = await bcrypt.hash(password, 12);
-    
+
     const result = await Admin.create({ username, password: hashedPassword });
 
     res
       .status(201)
       .json({ message: `L'admin ${username} a été créé avec succès`, result });
   } catch (err) {
-    res.status(400).send({ message: "Une erreur s'est produite" });
+    res.status(400).json({ message: "Une erreur s'est produite", err });
+    console.log('err', err);
+  }
+};
+
+export const logAdmin = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const existingAdmin = await Admin.findOne({ username });
+
+    const displayWrongCreds = () =>
+      res
+        .status(400)
+        .json({ message: 'Identifiant ou mot de passe incorrect' });
+
+    if (!existingAdmin) displayWrongCreds();
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingAdmin.password
+    );
+
+    if (!isPasswordCorrect) displayWrongCreds();
+
+    const token = jwt.sign({ username, id: existingAdmin._id }, secret, {
+      expiresIn: '1h',
+    });
+
+    res.status(200).json({ token });
+  } catch (err) {
+    res.status(400).json({ message: "Une erreur s'est produite", err });
     console.log('err', err);
   }
 };
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import { addAdmin } from '../controllers/admin.js';
+import { addAdmin, logAdmin } from '../controllers/admin.js';
 
 const adminRouter = express.Router();
 
 adminRouter.post('/add', addAdmin);
+adminRouter.post('/login', logAdmin);
 
 export default adminRouter;
